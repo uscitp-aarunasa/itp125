244ad07197fe8551ac678fb20b6714203bf27217
uscitp-aarunasa <ShoshanaPetunia@gmail.com>
2016-09-22 11:16:23 -0700

    Finally pushing via terminal

---
+++ lab02.py
@@ -0,0 +1,13 @@
+# Make sure that the_flying_circus() returns True
+def the_flying_circus():
+    if 5 == 5 and 5 != 6:
+        return True
+        # Start coding here!
+        # Don't forget to indent
+        # the code inside this block!
+    elif 6 ==6:
+        return True
+    else:
+        return False
+        # Keep going here.
+        # You'll want to add the else statement, too!
